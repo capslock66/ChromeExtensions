fc0ff58e20ba01c569266021f12c58c847d84650
capslock66 <mail.thierry.parent@gmail.com>
2017-01-08 23:08:10 +0100

    Fill popup from background generated table (only one time)

--- Page checker/js/background.js
+++ Page checker/js/background.js
@@ -1,5 +1,9 @@
 
 var scannerList = 111 ;
+var simpleCounter = 0 ;
+var pollInterval = 1000 * 30; // 1 minute, in milliseconds
+var timerId ;
+var resultTable;
 
 function main()
 {
@@ -11,10 +15,131 @@ function main()
     chrome.storage.sync.get('scannerList', function (obj) 
     { 
        scannerList = obj.scannerList;
-       console.log("get storage done : scanners : \n" , scannerList) ; 
+       console.log("get storage done : scanners : \n" , scannerList) ;
+       fillScannerTable() ;
+       timerId = window.setTimeout(startRequest, pollInterval);
     }) ; 
 
     console.log("background main end. ttrace.host : " + ttrace.host);
+}
+
+function fillScannerTable()
+{
+    //debugger;
+    resultTable = $(".scannerList_table");  
+   
+    for (var i in scannerList) 
+    {
+        var currentScanner = scannerList[i] ;        
+        var scannerTemplate = $('.scanner_div') ;
+        var scannerView = scannerTemplate.clone().removeClass("scanner_div").addClass('scanner_div2');
+
+        // Input name
+        var inputName = scannerView.find('.template_Name')[0] ;
+        inputName.scanner = currentScanner ;
+        currentScanner.inputName = inputName ;
+        currentScanner.inputName.value = currentScanner.Name ;
+        $(currentScanner.inputName).on("change keyup",function()   // change paste keyup
+        {
+            this.scanner.Name = $(this).val() ;
+            saveStorage();
+        }) ;
+
+        // Site
+        var inputSite = scannerView.find('.template_Site')[0] ;
+        inputSite.scanner = currentScanner ;
+        currentScanner.inputSite = inputSite ;
+        currentScanner.inputSite.value = currentScanner.Site ;
+        $(currentScanner.inputSite).on("change keyup",function()   // change paste keyup
+        {
+            this.scanner.Site = $(this).val() ;
+            saveStorage();
+        }) ;
+        
+        // SearchSelector
+        var inputSearchSelector = scannerView.find('.template_SearchSelector')[0] ;
+        inputSearchSelector.scanner = currentScanner ;
+        currentScanner.inputSearchSelector = inputSearchSelector ;
+        currentScanner.inputSearchSelector.value = currentScanner.SearchSelector ;
+        $(currentScanner.inputSearchSelector).on("change keyup",function()   // change paste keyup
+        {
+            this.scanner.SearchSelector = $(this).val() ;
+            saveStorage();
+        }) ;
+        
+        // template_CheckNow
+        var inputCheckNow = scannerView.find('.template_CheckNow')[0] ;
+        inputCheckNow.scanner = currentScanner ;
+        currentScanner.inputCheckNow = inputCheckNow ;
+        $(currentScanner.inputCheckNow ).click(function(){
+            doRequest(this.scanner);
+        }); 
+        
+        // Result
+        var inputResult = scannerView.find('.template_Result')[0] ;
+        inputResult.scanner = currentScanner ;
+        currentScanner.inputResult = inputResult ;
+        currentScanner.inputResult.innerText = "" ;
+        
+        // ArraySelector
+        var inputArraySelector = scannerView.find('.template_ArraySelector')[0] ;
+        inputArraySelector.scanner = currentScanner ;
+        currentScanner.inputArraySelector = inputArraySelector
+        currentScanner.inputArraySelector.value = currentScanner.ArraySelector ;
+        $(currentScanner.inputArraySelector).on("change keyup",function()   // change paste keyup
+        {
+            this.scanner.ArraySelector = $(this).val() ;
+            saveStorage();
+        }) ;
+        
+        // Enabled
+        var inputEnabled = scannerView.find('.template_Enabled')[0] ;
+        inputEnabled.scanner = currentScanner ;
+        currentScanner.inputEnabled = inputEnabled ;
+        $(currentScanner.inputEnabled).prop('checked',currentScanner.Enabled) ;
+        $(currentScanner.inputEnabled).on("change keyup",function()   // change paste keyup
+        {
+            this.scanner.Enabled = $(this).prop('checked');
+            saveStorage();
+        }) ;
+        
+        // Validated
+        var inputValidated = scannerView.find('.template_Validated')[0] ;
+        inputValidated.scanner = currentScanner ;
+        currentScanner.inputValidated = inputValidated ;
+        $(currentScanner.inputValidated).prop('checked',currentScanner.Validated) ;
+        $(currentScanner.inputValidated).on("change keyup",function()   // change paste keyup
+        {
+            this.scanner.Validated = $(this).prop('checked');
+            saveStorage();
+        }) ;
+        
+        // Hash  
+        var inputChecksum = scannerView.find('.template_Checksum')[0] ;
+        inputChecksum.scanner = inputChecksum ;
+        currentScanner.inputChecksum = inputChecksum ;
+        var hashToDisplay = currentScanner.Hash ;
+        if (currentScanner.newHash !== "undefined" && currentScanner.Hash !== currentScanner.newHash)
+            hashToDisplay = "" + currentScanner.newHash ;
+        currentScanner.inputChecksum.innerText = hashToDisplay ;        
+        
+        // CheckTime
+        var inputCheckTime = scannerView.find('.template_CheckTime')[0] ;
+        inputCheckTime.scanner = inputCheckTime ;
+        currentScanner.inputCheckTime = inputCheckTime ;
+        var CheckTime = currentScanner.CheckTime;
+        if (CheckTime == null)
+            CheckTime = "" ;
+        currentScanner.inputCheckTime.innerText = CheckTime ;              
+        
+        var ScannerTr = $("<tr></tr>");
+        var ScannerTd = $("<td></td>");
+        ScannerTr.append(ScannerTd);
+        ScannerTd.append(scannerView) ;
+        
+        resultTable.append(ScannerTr);
+        
+    }
 }
 
 function saveStorage()
@@ -42,7 +167,6 @@ function saveStorage()
         ttrace.debug.send("saveStorage done") ; 
     }) ;                             
 }
-
 
 function initStorage()
 {
@@ -149,5 +273,181 @@ function initStorage()
         ttrace.debug.send("initStorage done") ; 
     }) ;         
 }
+
+function startRequest() 
+{
+	doRequest();
+    timerId = window.setTimeout(startRequest, pollInterval);
+}
+
+function stopRequest() 
+{
+	window.clearTimeout(timerId);
+}
+
+function doRequest(specificScanner)
+{
+    simpleCounter ++ ;
+    console.log("doRequest") ;
+    ttrace.debug.send("doRequest");
+
+    var toScanCount = scannerList.length ;
+    if (specificScanner !== undefined)
+        toScanCount = 1 ;    
+    
+    var scannedCount = 0 ;
+    for (var i in scannerList) 
+    {
+        var currentScanner = scannerList[i] ;
+        if (specificScanner !== undefined && currentScanner !== specificScanner)
+            continue ; 
+        
+        // if specific scanner is used, don't check for enabled
+        if (specificScanner === undefined && currentScanner.Enabled === false)
+        {
+            toScanCount-- ;
+            continue ; 
+        }        
+        
+        currentScanner.index = i ;
+        var url = currentScanner.Site ;
+        var xhr = new XMLHttpRequest();
+        xhr.scanner = currentScanner ; // save to xhr for later retreival (onload callback) 
+
+        xhr.onload = function(e) 
+        {
+          // e : ProgressEvent
+          // e.currentTarget : XMLHttpRequest
+          // e.currentTarget.responseURL
+          var onloadRequest = e.currentTarget ;
+          var onLoadScanner = e.currentTarget.scanner ;
+          
+          // create an empty element, not stored in the document
+          var newDivElement = $('<div></div>' );
+        
+          // Parse the XMLHttpRequest result into the new element
+          newDivElement.html(onloadRequest.responseText);
+                
+          // search key in new element
+          var searchResults = $(onLoadScanner.SearchSelector, newDivElement);  
+          onLoadScanner.newHash = 0 ;  
+          
+          if (searchResults.length !== 0)
+          {
+              var ArraySelector = Number(onLoadScanner.ArraySelector) ;
+              
+              if (searchResults.length == 1)
+                  onLoadScanner.resultString = "1 result" ;
+              else
+                  onLoadScanner.resultString = "" + searchResults.length + " results. Specify Array Selection " ;
+              
+              for (var j = 0; j < searchResults.length; j++  )
+              {
+                  // print first 3 , the selected and the last one
+                  if (j < 3 || j === searchResults.length-1 || j === ArraySelector)                
+                      onLoadScanner.resultString = onLoadScanner.resultString + "\n" + "[" + j + "]" + searchResults[j].outerHTML ;
+                  else if (j === 3)
+                      onLoadScanner.resultString = onLoadScanner.resultString + "\n" + "..." ;
+              }
+                
+              var searchResult ;
+              if (ArraySelector === -1) 
+              {
+                  // take last
+                  var index = searchResults.length-1  ;  
+                  searchResult = searchResults[index].outerHTML ;                                        
+              } else if (ArraySelector === -2) { 
+                  // use count as a result
+                  searchResult = "" + searchResults.length ;
+              } else {
+                  // take the position
+                  if (ArraySelector < 0 || ArraySelector >= searchResults.length)
+                  {
+                      searchResult = "Out of range" ;
+                      onLoadScanner.resultString = onLoadScanner.resultString + "\n" + ArraySelector + " : Out of range" ;
+                  } else {
+                      searchResult = searchResults[ArraySelector].outerHTML  ;
+                  }
+              }  
+          } else {
+              searchResult = "Nothing !"; 
+              onLoadScanner.resultString  = "Nothing !";                        // view model : resultString
+          }           
+
+          onLoadScanner.newHash = searchResult.hashCode() ;   // view model : newHash                
+          
+          var hashToDisplay = onLoadScanner.newHash ;
+          if (onLoadScanner.Hash !== -1 && onLoadScanner.Hash !== onLoadScanner.newHash)
+          {
+              onLoadScanner.Validated = false ;      
+              $(onLoadScanner.inputValidated).prop('checked',false) ;              
+              hashToDisplay = "" + onLoadScanner.newHash ;
+              onLoadScanner.resultString = onLoadScanner.resultString + "\n" + "Page changed !!!" ;
+              chrome.browserAction.setBadgeText({text: '@@@'});
+          }
+          onLoadScanner.inputChecksum.innerText = hashToDisplay;                // view : hash          
+          onLoadScanner.inputResult.innerText = onLoadScanner.resultString ;    // view : result
+          
+          onLoadScanner.Hash = onLoadScanner.newHash ;                          // view model : hash
+          
+          var d = new Date ;
+          var dformat = [ d.getFullYear(),
+                         (d.getMonth()+1).padLeft(),
+                          d.getDate().padLeft()
+                        ].join('/')+ ' ' +
+                        [ d.getHours().padLeft(),
+                          d.getMinutes().padLeft(),
+                          d.getSeconds().padLeft()
+                        ].join(':');
+          
+          
+          onLoadScanner.CheckTime = dformat ;                                   // view model : CheckTime
+          onLoadScanner.inputCheckTime.innerText = onLoadScanner.CheckTime ;    // view : CheckTime
+
+          // save model
+          scannedCount++ ;
+          if (scannedCount == toScanCount || specificScanner !== undefined)
+          {
+              saveStorage() ;  
+              // display number of not validated scanner
+              var unvalidatedScanner = 0 ;
+              for (var j in scannerList) 
+              {
+                  var checkScanner = scannerList[j] ;
+                  if (checkScanner.Validated === false)
+                      unvalidatedScanner++ ;
+              }
+              if (unvalidatedScanner === 0)
+                  chrome.browserAction.setBadgeText({text:""});
+              else
+                  chrome.browserAction.setBadgeText({text:""+unvalidatedScanner});
+
+          }
+        } ;       
+        xhr.open("GET", url, true);         // xhrReq.open(method, url, async, user, password); 
+        xhr.send(null);                     // fire onload
+    }   
+}
+
+Number.prototype.padLeft = function(base,chr)
+{
+   var  len = (String(base || 10).length - String(this).length)+1;
+   return len > 0? new Array(len).join(chr || '0')+this : this;
+}
+
+String.prototype.hashCode = function()
+{
+    if (Array.prototype.reduce){
+        return this.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a;},0);              
+    } 
+    var hash = 0;
+    if (this.length === 0) return hash;
+    for (var i = 0; i < this.length; i++) {
+        var character  = this.charCodeAt(i);
+        hash  = ((hash<<5)-hash)+character;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+} ;
 
 main();
