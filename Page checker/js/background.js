878ba54cc49e52d9b1b71f035175d7169831b0e5
capslock66 <mail.thierry.parent@gmail.com>
2017-01-09 23:11:45 +0100

    add visual studio solution, fix popup element events

--- Page checker/js/background.js
+++ Page checker/js/background.js
@@ -1,6 +1,6 @@
 
 var scannerList = [] ;          // array of scanner
-var pollInterval = 1000 * 30;   // poll interval : 30 sec, in milliseconds
+var pollInterval = 1000 * 600;  // poll interval : 600 sec (10 minutes)
 var timerId ;                   // poll interval timer
 var resultTable;                // jquery for scannerList_table element
 
@@ -16,140 +16,16 @@ function main()
     ttrace.host = "localHost:85";
     //ttrace.debug.send("background init");
 
+    // ReSharper disable once UseOfImplicitGlobalInFunctionScope
     chrome.storage.sync.get('scannerList', function (obj) 
     { 
        scannerList = obj.scannerList;
        //console.log("get storage done : scanners : \n" , scannerList) ;
-       fillScannerTable() ;
        timerId = window.setTimeout(startRequest, pollInterval);
+       countUnValided();
     }) ; 
 
     console.log("background main end");
-}
-
-// called only one time when background is loaded
-function fillScannerTable()
-{
-    //debugger;
-    resultTable = $(".scannerList_table");  
-   
-    for (var i in scannerList) 
-    {
-        var currentScanner = scannerList[i] ;        
-        var ScannerTr = fillScanner(currentScanner) ;
-        resultTable.append(ScannerTr);        
-    }
-}
-
-// called by fillScannerTable or when a new scanner is added
-function fillScanner(currentScanner)
-{
-    var scannerTemplate = $('.scanner_div') ;
-    var scannerView = scannerTemplate.clone().removeClass("scanner_div").addClass('scanner_div2');
-
-    // Input name
-    var inputName = scannerView.find('.template_Name')[0] ;
-    inputName.scanner = currentScanner ;
-    currentScanner.inputName = inputName ;
-    currentScanner.inputName.value = currentScanner.Name ;
-    $(currentScanner.inputName).on("change keyup",function()   // change paste keyup
-    {
-        this.scanner.Name = $(this).val() ;
-        saveStorage();
-    }) ;
-
-    // Site
-    var inputSite = scannerView.find('.template_Site')[0] ;
-    inputSite.scanner = currentScanner ;
-    currentScanner.inputSite = inputSite ;
-    currentScanner.inputSite.value = currentScanner.Site ;
-    $(currentScanner.inputSite).on("change keyup",function()   // change paste keyup
-    {
-        this.scanner.Site = $(this).val() ;
-        saveStorage();
-    }) ;
-    
-    // SearchSelector
-    var inputSearchSelector = scannerView.find('.template_SearchSelector')[0] ;
-    inputSearchSelector.scanner = currentScanner ;
-    currentScanner.inputSearchSelector = inputSearchSelector ;
-    currentScanner.inputSearchSelector.value = currentScanner.SearchSelector ;
-    $(currentScanner.inputSearchSelector).on("change keyup",function()   // change paste keyup
-    {
-        this.scanner.SearchSelector = $(this).val() ;
-        saveStorage();
-    }) ;
-    
-    // template_CheckNow
-    var inputCheckNow = scannerView.find('.template_CheckNow')[0] ;
-    inputCheckNow.scanner = currentScanner ;
-    currentScanner.inputCheckNow = inputCheckNow ;
-    $(currentScanner.inputCheckNow ).click(function(){
-        doRequest(this.scanner);
-    }); 
-    
-    // Result
-    var inputResult = scannerView.find('.template_Result')[0] ;
-    inputResult.scanner = currentScanner ;
-    currentScanner.inputResult = inputResult ;
-    currentScanner.inputResult.innerText = "" ;
-    
-    // ArraySelector
-    var inputArraySelector = scannerView.find('.template_ArraySelector')[0] ;
-    inputArraySelector.scanner = currentScanner ;
-    currentScanner.inputArraySelector = inputArraySelector ;
-    currentScanner.inputArraySelector.value = currentScanner.ArraySelector ;
-    $(currentScanner.inputArraySelector).on("change keyup",function()   // change paste keyup
-    {
-        this.scanner.ArraySelector = $(this).val() ;
-        saveStorage();
-    }) ;
-    
-    // Enabled
-    var inputEnabled = scannerView.find('.template_Enabled')[0] ;
-    inputEnabled.scanner = currentScanner ;
-    currentScanner.inputEnabled = inputEnabled ;
-    $(currentScanner.inputEnabled).prop('checked',currentScanner.Enabled) ;
-    $(currentScanner.inputEnabled).on("change keyup",function()   // change paste keyup
-    {
-        this.scanner.Enabled = $(this).prop('checked');
-        saveStorage();
-    }) ;
-    
-    // Validated
-    var inputValidated = scannerView.find('.template_Validated')[0] ;
-    inputValidated.scanner = currentScanner ;
-    currentScanner.inputValidated = inputValidated ;
-    $(currentScanner.inputValidated).prop('checked',currentScanner.Validated) ;
-    $(currentScanner.inputValidated).on("change keyup",function()   // change paste keyup
-    {
-        this.scanner.Validated = $(this).prop('checked');
-        saveStorage();
-    }) ;
-    
-    // Hash  
-    var inputChecksum = scannerView.find('.template_Checksum')[0] ;
-    inputChecksum.scanner = inputChecksum ;
-    currentScanner.inputChecksum = inputChecksum ;
-    var hashToDisplay = currentScanner.Hash ;
-    if (currentScanner.newHash !== "undefined" && currentScanner.Hash !== currentScanner.newHash)
-        hashToDisplay = "" + currentScanner.newHash ;
-    currentScanner.inputChecksum.innerText = hashToDisplay ;        
-    
-    // CheckTime
-    var inputCheckTime = scannerView.find('.template_CheckTime')[0] ;
-    inputCheckTime.scanner = inputCheckTime ;
-    currentScanner.inputCheckTime = inputCheckTime ;
-    var CheckTime = currentScanner.CheckTime;
-    if (CheckTime === null || CheckTime === undefined)
-        CheckTime = "" ;
-    currentScanner.inputCheckTime.innerText = CheckTime ;              
-    
-    var ScannerTr = $("<tr></tr>");
-    var ScannerTd = $("<td></td>");
-    ScannerTr.append(ScannerTd);
-    ScannerTd.append(scannerView) ;
-    return ScannerTr ;
 }
 
 // save scanner list
@@ -157,7 +33,7 @@ function saveStorage()
 {
     // create a copy of the scannerList to save only needed fields
     var scannerCopyList = [] ;
-    for (var i in scannerList) 
+    for (let i = 0; i < scannerList.length;i++)
     {
         var scanner = scannerList[i] ;
         var scannerCopy = {} ;
@@ -172,7 +48,8 @@ function saveStorage()
         scannerCopyList.push(scannerCopy);        
     }
     
-    chrome.storage.sync.set({'scannerList': scannerCopyList}, function (obj) 
+    // ReSharper disable once UseOfImplicitGlobalInFunctionScope
+    chrome.storage.sync.set({'scannerList': scannerCopyList}, function () 
     {
         //console.log("saveStorage done") ;
         //ttrace.debug.send("saveStorage done") ; 
@@ -215,9 +92,9 @@ function requestCallBack (e)
     
     if (searchResults.length !== 0)
     {
-        var ArraySelector = Number(onLoadScanner.ArraySelector) ;
+        var arraySelector = Number(onLoadScanner.ArraySelector) ;
         
-        if (searchResults.length == 1)
+        if (searchResults.length === 1)
             onLoadScanner.resultString = "1 result" ;
         else
             onLoadScanner.resultString = "" + searchResults.length + " results. Specify Array Selection " ;
@@ -225,28 +102,28 @@ function requestCallBack (e)
         for (var j = 0; j < searchResults.length; j++  )
         {
             // print first 3 , the selected and the last one
-            if (j < 3 || j === searchResults.length-1 || j === ArraySelector)                
+            if (j < 3 || j === searchResults.length-1 || j === arraySelector)                
                 onLoadScanner.resultString = onLoadScanner.resultString + "\n" + "[" + j + "]" + searchResults[j].outerHTML ;
             else if (j === 3)
                 onLoadScanner.resultString = onLoadScanner.resultString + "\n" + "..." ;
         }
           
-        if (ArraySelector === -1) 
+        if (arraySelector === -1) 
         {
             // take last
             var index = searchResults.length-1  ;  
             searchResult = searchResults[index].outerHTML ;                                        
-        } else if (ArraySelector === -2) { 
+        } else if (arraySelector === -2) { 
             // use count as a result
             searchResult = "" + searchResults.length ;
         } else {
             // take the position
-            if (ArraySelector < 0 || ArraySelector >= searchResults.length)
+            if (arraySelector < 0 || arraySelector >= searchResults.length)
             {
                 searchResult = "Out of range" ;
-                onLoadScanner.resultString = onLoadScanner.resultString + "\n" + ArraySelector + " : Out of range" ;
+                onLoadScanner.resultString = onLoadScanner.resultString + "\n" + arraySelector + " : Out of range" ;
             } else {
-                searchResult = searchResults[ArraySelector].outerHTML  ;
+                searchResult = searchResults[arraySelector].outerHTML  ;
             }
         }  
     } else {
@@ -254,55 +131,68 @@ function requestCallBack (e)
         onLoadScanner.resultString  = "Nothing !";                        // view model : resultString
     }           
   
-    onLoadScanner.newHash = searchResult.hashCode() ;   // view model : newHash                
+    onLoadScanner.newHash = hashCode(searchResult) ;   // view model : newHash                
     
     var hashToDisplay = onLoadScanner.newHash ;
     if (onLoadScanner.Hash !== -1 && onLoadScanner.Hash !== onLoadScanner.newHash)
     {
-        onLoadScanner.Validated = false ;      
-        $(onLoadScanner.inputValidated).prop('checked',false) ;              
+        onLoadScanner.Validated = false;
+        if (onLoadScanner.inputValidated !== undefined)
+            $(onLoadScanner.inputValidated).prop('checked',false) ;              
         hashToDisplay = "" + onLoadScanner.newHash ;
         onLoadScanner.resultString = onLoadScanner.resultString + "\n" + "Page changed !!!" ;
         needToBeSaved = true ;
     }
-    onLoadScanner.inputChecksum.innerText = hashToDisplay;                // view : hash          
-    onLoadScanner.inputResult.innerText = onLoadScanner.resultString ;    // view : result
+
+    if (onLoadScanner.inputChecksum !== undefined)
+        onLoadScanner.inputChecksum.innerText = hashToDisplay;                // view : hash     
+
+    if (onLoadScanner.inputResult !== undefined)
+        onLoadScanner.inputResult.innerText = onLoadScanner.resultString;     // view : result
     
     onLoadScanner.Hash = onLoadScanner.newHash ;                          // view model : hash
     
     var d = new Date() ;
     var dformat = [ d.getFullYear(),
-                   (d.getMonth()+1).padLeft(),
-                    d.getDate().padLeft()
+                   padLeft(d.getMonth()+1),
+                   padLeft(d.getDate())
                   ].join('/')+ ' ' +
-                  [ d.getHours().padLeft(),
-                    d.getMinutes().padLeft(),
-                    d.getSeconds().padLeft()
+                  [ padLeft(d.getHours()),
+                    padLeft(d.getMinutes()),
+                    padLeft(d.getSeconds())
                   ].join(':');
     
     
     onLoadScanner.CheckTime = dformat ;                                   // view model : CheckTime
-    onLoadScanner.inputCheckTime.innerText = onLoadScanner.CheckTime ;    // view : CheckTime
+    if (onLoadScanner.inputCheckTime !== undefined)
+        onLoadScanner.inputCheckTime.innerText = onLoadScanner.CheckTime;    // view : CheckTime
   
     // save model
     scannedCount++ ;
-    if (scannedCount == toScanCount )   // || specificScanner !== undefined
+    if (scannedCount === toScanCount )   // || specificScanner !== undefined
     {
         if (needToBeSaved)  
             saveStorage() ;  
         // display number of unvalidated (and enabled) scanner 
-        var unvalidatedScanner = 0 ;
-        for (var k in scannerList) 
-        {
-            var checkScanner = scannerList[k] ;
-            if (checkScanner.Validated === false && checkScanner.Enabled === true)
-                unvalidatedScanner++ ;
-        }
-        if (unvalidatedScanner === 0)
-            chrome.browserAction.setBadgeText({text:""});
-        else
-            chrome.browserAction.setBadgeText({text:""+unvalidatedScanner});
-  
+        countUnValided() ;
+    }
+}
+
+function countUnValided()
+{
+    var unvalidatedScanner = 0 ;
+    for (let k = 0; k < scannerList.length;k++)
+    {
+        var checkScanner = scannerList[k] ;
+        if (checkScanner.Validated === false && checkScanner.Enabled === true)
+            unvalidatedScanner++ ;
+    }
+    if (unvalidatedScanner === 0) {
+        // ReSharper disable once UseOfImplicitGlobalInFunctionScope
+        chrome.browserAction.setBadgeText({ text: "" });
+    } else {
+        // ReSharper disable once UseOfImplicitGlobalInFunctionScope
+        chrome.browserAction.setBadgeText({ text: "" + unvalidatedScanner });
     }
 }
 
@@ -310,7 +200,7 @@ function requestCallBack (e)
 // Asynchrone. requestCallBack will be called for each one
 function doRequest(specificScanner)
 {
-    //console.log("doRequest") ;
+    console.log("doRequest") ;
     //ttrace.debug.send("doRequest");
 
     toScanCount = scannerList.length ;
@@ -319,7 +209,7 @@ function doRequest(specificScanner)
     
     scannedCount = 0 ;
     needToBeSaved = false ;
-    for (var i in scannerList) 
+    for (let i = 0; i < scannerList.length; i++)
     {
         var currentScanner = scannerList[i] ;
         if (specificScanner !== undefined && currentScanner !== specificScanner)
@@ -334,6 +224,7 @@ function doRequest(specificScanner)
         
         currentScanner.index = i ;
         var url = currentScanner.Site ;
+        // ReSharper disable once InconsistentNaming
         var xhr = new XMLHttpRequest();
         xhr.scanner = currentScanner ; // save to xhr for later retreival (onload callback) 
 
@@ -444,7 +335,8 @@ function initStorage()
     scannerList.push(scanner);
     
     console.log ("save all scanner") ;
-    chrome.storage.sync.set({'scannerList': scannerList}, function (obj) 
+     // ReSharper disable once UseOfImplicitGlobalInFunctionScope
+    chrome.storage.sync.set({'scannerList': scannerList}, function () //obj
     {
         //console.log("initStorage done") ;
         //ttrace.debug.send("initStorage done") ; 
@@ -452,22 +344,23 @@ function initStorage()
 }
 
 // Helper
-Number.prototype.padLeft = function(base,chr)
+function padLeft (src,base,chr)
 {
-   var  len = (String(base || 10).length - String(this).length)+1;
-   return len > 0? new Array(len).join(chr || '0')+this : this;
+   var len = (String(base || 10).length - String(src).length) + 1;
+   var result = len > 0 ? new Array(len).join(chr || '0') + src : src;
+   return result ;
 };
 
 // Helper
-String.prototype.hashCode = function()
+function hashCode (src)
 {
     if (Array.prototype.reduce){
-        return this.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a;},0);              
+       return src.split("").reduce(function (a, b) { a = ((a << 5) - a) + b.charCodeAt(0); return a & a; }, 0);
     } 
     var hash = 0;
-    if (this.length === 0) return hash;
-    for (var i = 0; i < this.length; i++) {
-        var character  = this.charCodeAt(i);
+    if (src.length === 0) return hash;
+    for (var i = 0; i < src.length; i++) {
+        var character = src.charCodeAt(i);
         hash  = ((hash<<5)-hash)+character;
         hash = hash & hash; // Convert to 32bit integer
     }
--- Page checker/js/popup.js
+++ Page checker/js/popup.js
@@ -1,28 +1,196 @@
 var ttrace ;
+var resultTable;                // jquery for scannerList_table element
+var backgroundPage;
 
 function init() 
 {
-    var backgroundPage = chrome.extension.getBackgroundPage() ;
-    ttrace = backgroundPage.ttrace ;
-    //console.log("popup init") ;   
+    // ReSharper disable once UseOfImplicitGlobalInFunctionScope
+    backgroundPage = chrome.extension.getBackgroundPage() ;
+    ttrace = backgroundPage.ttrace;
+
+   // don't use the popup console. Use the backgroundPage console
+    backgroundPage.console.log("popup init");
     //ttrace.debug.send("popup init " );
 
     
     $(document).ready(function() 
     {
-        $("#check_request_button").click(function(){
-           backgroundPage.doRequest();
+        $("#check_request_button").click(function ()
+        {
+            backgroundPage.doRequest();
         }); 
-        
-        $("#init_storage_button").click(function () {
-           backgroundPage.initStorage();
+         
+        $("#init_storage_button").click(function ()
+        {
+            backgroundPage.initStorage();
         });
-        
-        // scanner table list View is already created in background and stored in backgroundPage.resultTable.
-        $("#response_body").append (backgroundPage.resultTable) ;
+
+        addEventListener("unload", function (event)
+        {
+           backgroundPage.console.log(event.type);
+        }, true);
+
+        fillScannerTable();
     });
 }
 
+function fillScannerTable()
+{
+    //var responseBody = $("#response_body");
+    resultTable = $(".scannerList_table");
+ 
+    for (let i = 0; i < backgroundPage.scannerList.length; i++)
+    {
+        var currentScanner = backgroundPage.scannerList[i];
+        var scannerTr = CloneScannerTemplate(currentScanner);
+        SetScannerEvents(currentScanner);
+        resultTable.append(scannerTr);
+    }
+    //responseBody.append(resultTable);
+}
+
+// called by fillScannerTable or when a new scanner is added
+function CloneScannerTemplate(currentScanner) {
+   var scannerTemplate = $(".scanner_div");
+   var scannerView = scannerTemplate.clone().removeClass("scanner_div").addClass("scanner_div2");
+
+   // Input name
+   var inputName = scannerView.find(".template_Name")[0];
+   inputName.scanner = currentScanner;
+   currentScanner.inputName = inputName;
+   currentScanner.inputName.value = currentScanner.Name;
+
+   // Site
+   var inputSite = scannerView.find(".template_Site")[0];
+   inputSite.scanner = currentScanner;
+   currentScanner.inputSite = inputSite;
+   currentScanner.inputSite.value = currentScanner.Site;
+
+   // SearchSelector
+   var inputSearchSelector = scannerView.find(".template_SearchSelector")[0];
+   inputSearchSelector.scanner = currentScanner;
+   currentScanner.inputSearchSelector = inputSearchSelector;
+   currentScanner.inputSearchSelector.value = currentScanner.SearchSelector;
+
+   // template_CheckNow
+   var inputCheckNow = scannerView.find(".template_CheckNow")[0];
+   inputCheckNow.scanner = currentScanner;
+   currentScanner.inputCheckNow = inputCheckNow;
+
+   // Result
+   var inputResult = scannerView.find(".template_Result")[0];
+   inputResult.scanner = currentScanner;
+   currentScanner.inputResult = inputResult;
+   currentScanner.inputResult.innerText = "";
+
+   // ArraySelector
+   var inputArraySelector = scannerView.find(".template_ArraySelector")[0];
+   inputArraySelector.scanner = currentScanner;
+   currentScanner.inputArraySelector = inputArraySelector;
+   currentScanner.inputArraySelector.value = currentScanner.ArraySelector;
+
+   // Enabled
+   var inputEnabled = scannerView.find(".template_Enabled")[0];
+   inputEnabled.scanner = currentScanner;
+   currentScanner.inputEnabled = inputEnabled;
+   $(currentScanner.inputEnabled).prop("checked", currentScanner.Enabled);
+
+   // Validated
+   var inputValidated = scannerView.find(".template_Validated")[0];
+   inputValidated.scanner = currentScanner;
+   currentScanner.inputValidated = inputValidated;
+   $(currentScanner.inputValidated).prop("checked", currentScanner.Validated);
+
+   // Hash  
+   var inputChecksum = scannerView.find(".template_Checksum")[0];
+   inputChecksum.scanner = inputChecksum;
+   currentScanner.inputChecksum = inputChecksum;
+   var hashToDisplay = currentScanner.Hash;
+   if (currentScanner.newHash !== "undefined" && currentScanner.Hash !== currentScanner.newHash)
+      hashToDisplay = "" + currentScanner.newHash;
+   currentScanner.inputChecksum.innerText = hashToDisplay;
+
+   // CheckTime
+   var inputCheckTime = scannerView.find(".template_CheckTime")[0];
+   inputCheckTime.scanner = inputCheckTime;
+   currentScanner.inputCheckTime = inputCheckTime;
+   var checkTime = currentScanner.CheckTime;
+   if (checkTime === null || checkTime === undefined)
+      checkTime = "";
+   currentScanner.inputCheckTime.innerText = checkTime;
+
+   var scannerTr = $("<tr></tr>");
+   var scannerTd = $("<td></td>");
+   scannerTr.append(scannerTd);
+   scannerTd.append(scannerView);
+   return scannerTr;
+}
+
+function SetScannerEvents(currentScanner)
+{
+   // Input name
+   $(currentScanner.inputName).on("change keyup", function ()   // change paste keyup
+   {
+      this.scanner.Name = $(this).val();
+      backgroundPage.saveStorage();
+   });
+
+   // Site
+   $(currentScanner.inputSite).on("change keyup", function ()   // change paste keyup
+   {
+      this.scanner.Site = $(this).val();
+      backgroundPage.saveStorage();
+   });
+
+   // SearchSelector
+   $(currentScanner.inputSearchSelector).on("change keyup", function ()   // change paste keyup
+   {
+      this.scanner.SearchSelector = $(this).val();
+      backgroundPage.saveStorage();
+   });
+
+   // template_CheckNow
+   $(currentScanner.inputCheckNow).click(function () {
+      backgroundPage.doRequest(this.scanner);
+   });
+
+   // Result 
+   // no event...
+
+   // ArraySelector
+   $(currentScanner.inputArraySelector).on("change keyup", function ()   // change paste keyup
+   {
+      this.scanner.ArraySelector = $(this).val();
+      backgroundPage.saveStorage();
+   });
+
+   // Enabled
+   $(currentScanner.inputEnabled).on("change keyup", function ()   // change paste keyup
+   {
+      this.scanner.Enabled = $(this).prop('checked');
+      backgroundPage.saveStorage();
+      // display number of unvalidated (and enabled) scanner 
+      backgroundPage.countUnValided();
+      backgroundPage.console.log("popup inputEnabled changed");
+   });
+
+   // Validated
+   $(currentScanner.inputValidated).on("change keyup", function ()   // change paste keyup
+   {
+      this.scanner.Validated = $(this).prop('checked');
+      backgroundPage.saveStorage();
+      // display number of unvalidated (and enabled) scanner 
+      backgroundPage.countUnValided();
+      backgroundPage.console.log("popup inputValidated changed");
+   });
+
+   // Hash  
+   // no event...
+
+   // CheckTime
+   // no event...
+
+}
 
 init();
 
